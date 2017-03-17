a509ea943f4eb5345f63344164abaeee62ff9e6e
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-03-17 11:24:52 -0300

    Output to standard output.

--- dbf2sql.sh
+++ dbf2sql.sh
@@ -2,30 +2,30 @@
 #
 # DBF to standard SQL converter
 #
+# Usage:
+#
+#   dbf2sql.sh YOURFILE.DBF > YOURFILE.SQL
+#
 
 file=$1
-output_dir=output
-
-[ -d $output_dir ] || mkdir $output_dir
-
-> $output_dir/input_files.sha
-> $output_dir/output_files.sha
 
 file_name=$( basename $file | sed -ne 's/\(.*\).DBF/\1/p' )
-file_sql="$file_name".sql
-
-## Compute the SHA
-shasum $file >> $output_dir/input_files.sha
 
 ## Generate the CREATE TABLE
 dbf_dump --info --SQL $file | \
-  sed 's/create table \([^[:space:]]\+\)/CREATE TABLE "\1"/g' >> $output_dir/$file_sql
+  # Add "" to table name for names that start with numbers.
+  sed 's/create table \([^[:space:]]\+\)/CREATE TABLE "\1"/g' | \
+  # Replace invalid SQL data types
+  sed 's/date([0-9]\+)/date/g'
 
-echo ';' >> $output_dir/$file_sql
+echo ';'
 
 ## Generate the INSERTs
-dbf_dump --info $file | tail -n +10 | sed -ne 's/^\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)$/\2/p' | xargs | sed -ne 's/[[:space:]]/,/g;s/\(.*\)/INSERT INTO '$file_name'(\1) VALUES /p' \
-  >> $output_dir/$file_sql
+dbf_dump --info $file | \
+  # Filter for columns detail
+  grep '^[0-9]\+\.' | \
+  # Generate INSERT statements
+  sed -ne 's/^\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)[[:space:]]\+\([^[:space:]]\+\)$/\2/p' | xargs | sed -ne 's/[[:space:]]/,/g;s/\(.*\)/INSERT INTO "'$file_name'"(\1) VALUES /p'
 
 ## Generate the VALUEs
 dbf_dump --undef NULL --fs '@@SEP@@' $file | \
@@ -35,8 +35,4 @@ dbf_dump --undef NULL --fs '@@SEP@@' $file | \
   sed "s/@@SEP@@/', '/g;s/^/@@BEGIN@@('/g;s/$/')@@END@@/g;" | \
   sed "s/'NULL'/NULL/g" | \
   sed ':a;N;$!ba;s/\n/ /g' | \
-  sed 's/@@END@@\s*@@BEGIN@@/, /g;s/@@BEGIN@@//g;s/@@END@@/;/g' \
-    >> $output_dir/$file_sql
-
-## Compute the SHA for CSV output
-shasum $output_dir/$file_sql >> $output_dir/output_files.sha
+  sed 's/@@END@@\s*@@BEGIN@@/, /g;s/@@BEGIN@@//g;s/@@END@@/;/g'
